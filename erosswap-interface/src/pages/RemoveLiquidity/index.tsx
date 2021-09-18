0e973d058b4b6496a43d36ea763825cb24a9c853
hero007asd <417732702@qq.com>
2021-09-18 19:58:59 +0800

    fix bug: remove liquidity error

--- erosswap-interface/src/pages/RemoveLiquidity/index.tsx
+++ erosswap-interface/src/pages/RemoveLiquidity/index.tsx
@@ -123,7 +123,7 @@ export default function RemoveLiquidity({
       { name: 'verifyingContract', type: 'address' }
     ]
     const domain = {
-      name: 'Erosswap V2',
+      name: 'Uniswap V2',
       version: '1',
       chainId: chainId,
       verifyingContract: pair.liquidityToken.address
