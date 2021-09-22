82d99b898dc4df93c6a96743bbdb8673a4b969a3
hero007asd <417732702@qq.com>
2021-09-22 14:39:55 +0800

    update ROUTER_ADDRESS to  fix create pair error

--- erosswap-interface/src/constants/index.ts
+++ erosswap-interface/src/constants/index.ts
@@ -4,7 +4,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 // update 20210915
 // export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
-export const ROUTER_ADDRESS = '0x397a3356a93ababab475f53d77d2e26250ae0e6b'
+export const ROUTER_ADDRESS = '0x316759c4245ef81643d15d1665b6c22a8b04338c'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
